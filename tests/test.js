1749bc80ce38c4d727a0a2c2f4b8ee98ed6e2b5d
Esinko <34211401+Esinko@users.noreply.github.com>
2020-08-18 20:50:27 +0300

    Delete test.js

--- tests/test.js
+++
@@ -1,8 +0,0 @@
-const OpenWilma = require("../OpenWilma.js")
-let session = new OpenWilma()
-
-session._checkUrl().then(async res => {
-    console.log("Compatible runtime!")
-}).catch(async err => {
-    console.log("Unsupported runtime: " + err)
-})
