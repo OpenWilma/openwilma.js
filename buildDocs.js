908fe787e2a7b851fcde4747f2913e7afad374d2
Esinko <34211401+Esinko@users.noreply.github.com>
2021-01-01 21:54:55 +0200

    Fix typo in docs

--- buildDocs.js
+++ buildDocs.js
@@ -5,7 +5,7 @@ jsdoc2md.render({ files: "*.js" }).then(async data => {
     data = data.split('<a name="OpenWilma"></a>')
     data[0] = '# Table of contents\n' + data[0].trimLeft()
     data[1] = data[1].replace(/(^)## /gm, "---\n## ")
-    data = data.join("\n# Documentation\nDefenitions for each item. *see Table of contents*<br>")
+    data = data.join("\n# Documentation\nDefenitions for each item (*see Table of contents*).<br>")
     data = data + "\n---\n# End\nThese docs where generated with the ```buildDocs.js``` and can be easily regenerated with ```npm run build``` if there have been any changes to the Source Code"
     fs.writeFileSync("./Docs.md", data)
     console.log("Docs built")
