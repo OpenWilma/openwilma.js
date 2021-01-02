c61317d18599147aed98a0d2e215515152779777
Esinko <34211401+Esinko@users.noreply.github.com>
2021-01-02 11:19:19 +0200

    Fix typos

--- buildDocs.js
+++ buildDocs.js
@@ -6,7 +6,7 @@ jsdoc2md.render({ files: "*.js" }).then(async data => {
     data[0] = '# Table of contents\n' + data[0].trimLeft()
     data[1] = data[1].replace(/(^)## /gm, "---\n## ")
     data = data.join("\n# Documentation\nDefenitions for each item (*see Table of contents*).<br>")
-    data = data + "\n---\n# End\nThese docs where generated with the ```buildDocs.js``` and can be easily regenerated with ```npm run build``` if there have been any changes to the Source Code."
+    data = data + "\n---\n# End\nThese docs were generated with```buildDocs.js``` and can be easily regenerated with ```npm run build``` if there have been any changes to the Source Code."
     fs.writeFileSync("./Docs.md", data)
     console.log("Docs built")
 })
