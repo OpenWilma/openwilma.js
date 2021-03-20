4640aa3def0bdea603fbff6cbe9029ec6586289e
developerfromjokela <developerfromjokela@gmail.com>
2021-03-20 12:49:49 +0200

    Added test command to npm scripts for easy testing, and moved testing credentials to its separate file instead of command arguments

--- test/test.js
+++ test/test.js
@@ -1,20 +1,14 @@
 // Module testing, CommonJS
 let op = require("../dist/index.js")
 
-// Get cred
-const username = process.argv[2]
-const password = process.argv[3]
-const server = process.argv[4]
+// Get creds, remember to copy example file to test.config.json!
+const testCredentials = require("./test.config.json");
 
 const ses = new op.client()
 
 // akuli TestiSalasana <server ;)>
 
-ses.login({
-	username: username,
-	password: password,
-	server: server
-}, false).then(async account => {
+ses.login(testCredentials, false).then(async account => {
 	console.log("Logged in")
 	console.log(account)
 	let exams = await account.exams.list()
